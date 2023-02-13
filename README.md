449bb6cde249e51f918964345675f901ae7adc93
Arkadiusz Halicki <arek@MacBook-Pro-Arkadiusz.local>
2023-02-13 11:19:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WaterLand
+
+`waterland` is an Python app for calculating the number of 1s islands on a 2D array of 1s and 0s.
---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="allocation",
+    version="0.1",
+    packages=["waterland"],
+    install_requires=[
+        "click==8.1.3",
+    ],
+)
