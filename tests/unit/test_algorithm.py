55641657544f7a97a3b88e2361f1d01be03d1bf5
Arkadiusz Halicki <halicki.arkadiusz@gmail.com>
2023-02-14 11:12:13 +0100

    Add more tests

--- tests/unit/test_algorithm.py
+++ tests/unit/test_algorithm.py
@@ -1,5 +1,22 @@
+import pytest
+
 import waterland.algorithm
 
 
 def test_algorithm(grid):
     assert waterland.algorithm.count_islands(grid) == 4
+
+
+@pytest.mark.parametrize(
+    "grid, expected",
+    [
+        ([[0, 0]], 0),
+        ([[0, 1]], 1),
+        ([[1, 1]], 1),
+        ([[0, 1], [0, 0]], 1),
+        ([[1, 0], [0, 1]], 1),
+        ([[1, 1], [1, 0]], 1),
+    ],
+)
+def test_algorithm2(grid, expected):
+    assert waterland.algorithm.count_islands(grid) == expected
